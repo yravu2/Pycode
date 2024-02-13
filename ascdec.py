39dd111bb60829a35508075c15d7ccab67da9057
yravu2 <mail2yaminiravuri111@gmail.com>
2024-02-13 15:48:29 -0600

    fourth commit

---
+++ ascdec.py
@@ -0,0 +1,20 @@
+number=[1,5,8,77,11,34,6,10]
+
+number.sort()
+print('List in Ascending Order: ', number)
+number.sort(reverse=True)
+print('List in Descending Order: ', number)
+
+al=['a','h','t','e','b','c','d','i']
+al.sort()
+print('List in Ascending Order: ', al)
+al.sort(reverse=True)
+print('List in Descending Order: ', al)
+
+color=['blue','red','green','yellow']
+color.sort()
+print('List in Ascending Order: ', color)
+color.sort(reverse=True)
+print('List in Descending Order: ', color)
+
+
